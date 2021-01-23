2b3cd0afb4f6a9fea0b932581630eedf74fddff7
chutchaikp <chutchai.kp@gmail.com>
2021-01-23 12:43:04 +0700

    Init Solution

---
+++ c-sharp-redis/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_redis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi there .");
+        }
+    }
+}
