3cb0ae245d5505c13c8a32bcfb10a1bcc11f2994
chutchaikp <chutchai.kp@gmail.com>
2021-05-14 17:54:11 +0700

    Hello C# Async

---
+++ c-sharp-async/Main.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace c_sharp_async
+{
+    public partial class Main : Form
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+
+        public Main()
+        {
+            InitializeComponent();
+
+            log.Debug("Hello Async");
+
+        }
+    }
+}
